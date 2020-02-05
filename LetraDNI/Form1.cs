478ca6513c1cf899754c63a3544944a38630c005
HectorGGBABY <hectorgarill@monlau.com>
2020-02-05 16:39:58 +0100

    VERSION 0.0.1

---
+++ LetraDNI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetraDNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dni = Convert.ToInt32(textBoxDNI.Text);
+            char letter = getletter();
+            textBoxletra.Text = letter.ToString();
+        }
+
+        private char getletter()
+        {
+            char a = 'a';
+
+            return a;
+            
+        }
+    }
+}
