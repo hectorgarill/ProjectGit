862703ea200ff2b0f1ee04a8b3c4907b7f157d28
Diosito El Hector <Hectorgarill@monlau.com>
2020-02-12 15:56:20 +0100

    Version 0.0.1

--- LetraDNI/Form1.cs
+++ LetraDNI/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace LetraDNI
 {
     public partial class Form1 : Form
     {
+        string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
         public Form1()
         {
             InitializeComponent();
@@ -20,16 +22,18 @@ namespace LetraDNI
         private void button1_Click(object sender, EventArgs e)
         {
             int dni = Convert.ToInt32(textBoxDNI.Text);
-            char letter = getletter();
-            textBoxletra.Text = letter.ToString();
+
+            char[] letter = getletter(dni);
+
+            textBoxletra.Text = letter[0].ToString();
         }
 
-        private char getletter()
+        private char[] getletter(int dni)
         {
-            char a = 'a';
+            char[] letra=letras.ToCharArray(dni%23,1);
 
-            return a;
-            
+            return letra;
+
         }
     }
 }
